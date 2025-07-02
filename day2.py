47835e245f26701bf316ccdc253423a682a92a13
Maneev Arora <modak.arora.7@gmail.com>
2025-07-02 17:59:24 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,23 @@
+# A-Z, a-z, 0-9, special symbols #!@%$
+#character set in python
+# print("HELLO WORLD")
+# print("MANEEV ARORA")
+# age=2000
+# name="Maneev Arora"
+# print("My name is",name, "and my age is", age)
+# a=78
+# b=90
+# sum=a+b
+# name="himank"
+# print(name)
+# a=90
+# b=1890
+# sum=a+b
+# print("The sum of",a,"&",b,"is",sum)
+# print('himank')
+print("my name is himank vaid","and my age is 19")
+a=7
+#helloworld
+
+"""Hello Himank"""
+# print("and my age is 19")
