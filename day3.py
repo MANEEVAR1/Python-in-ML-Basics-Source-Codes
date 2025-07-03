dfef08fdb7981831daaa35c204ddf7a008022f49
Maneev Arora <modak.arora.7@gmail.com>
2025-07-03 17:57:52 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,32 @@
+# variables
+a=90
+# variables names cannot start with a number, they must only start with an alphabet or an underscore.
+_name1="maneev arora"
+_name2="modak"
+print(_name1)
+print(_name2)
+# python supports right to left variable assignment
+# data types: int(integer), float(floating point value(decimal numbers)), str(string), bool(boolean)
+a=-90000000000000000000000000000000000000000
+print(type(a))
+f=3.14
+print(type(f))
+s="hello rishi"
+print(type(s))
+b=False
+print(type(b))
+# identifiers are names used for variables, functions, etc
+# we can use digits, letters and underscores in our identifier names (0-9, A-Z, a-z, _)
+# identifier names can't start with a digit
+# identifier names cannot be keywords
+# identifier names are case sensitive meaning they distinguish between uppercase and lowercase letters
+Var1="shriya"
+var1="shriya"
+# keywords are reserved words in python which are used by python itself. we cannot use keywords in identifiers, variables, function names, etc examples: for, while, True, False, if, else, etc....
+# import keyword
+# print(keyword.kwlist)
+# punctuator are symbols which help us structure our code better examples:[], {}, (), #
+
+# questions
+# Q1.HERE ARE SOME VARIABLE AND IDENTIFIER NAMES, EVALUATE CORRECT AND WRONG NAMES AND SPECIFY THE REASON FOR YOUR CHOICE. WORDS: first_name, _temp2, 78_temp, hellloworld21, 1world1_hello
+# Q2. MAKE ANY 5 VARIABLES AND ASSIGN VALUES TO THEM AND SHOW THEIR DATA TYPES IN THE TERMINAL.
