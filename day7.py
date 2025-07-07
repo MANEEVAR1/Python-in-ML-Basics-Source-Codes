04d7df658afb7bf4164aa7d744a7659abf870d01
Maneev Arora <modak.arora.7@gmail.com>
2025-07-07 18:11:56 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,40 @@
+# tuples are a data type in python used for storing data in paranthesis (). It is a way of making an ordered collection of elements in python. We can store data of multiple types eg:int, str, float, char,etc...Whereas lists are mutable in python but tuples are an immmutable data type. which means that we cannot  make any changes once we create them just like strings.
+
+tup=1,2,3,4,5
+# when we store our data in tuples by using a paranthesis() this concept is called 'Tuple Packing'
+print(type(tup), tup)
+tup1=("rajeev",24,87.90, False)
+stu=("Aman",16, 56.9, False)
+str="I love Python"
+tup1[0]="Vivek"
+# str[2]="d"
+print(str)
+print(tup1)
+# data consistency and integrity- which means that once a tuple is created, it cannot be altered/changed/updated.
+# tuples are faster processing as compared to lists
+#semantic clarity enforces the behaviour of tuple i.e immutability.
+# empty tuple
+tupp=()
+print(type(tupp))
+# tuple indexing
+tup1=(24,87.90, False,"rajeev")
+print(tup1[-4])
+# tuple slicing
+print(tup1[0:4])
+# tuple length
+print(len(tup1))
+# tuple concetenation
+tup2=tup1+tupp
+print(tup2)
+# tuple repetition
+print(tup2*8)
+#tuple membership operators
+print(4 not in tup1)
+# tuple operations
+# 1. count-->counts the occurence of an element in our data structure.
+# 2. index--> used for checking the index of a value
+print(tup2.count("rajeev"))
+print(tup2.index("rajeev"))
+# nested tuples
+tup3=(23, 56, 45,(34,98,20))
+print(tup3[3][1])
